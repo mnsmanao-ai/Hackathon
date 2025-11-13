67f437e0c3d1e11726be8af30b97263d85c0cefa
Lucie Blr <florian.xaxa@epitech.eu>
2025-11-13 17:26:52 +0100

    update API endpoint in contacts.js to use backend service and modify Docker configuration for backend service

--- src/api/contacts.js
+++ src/api/contacts.js
@@ -1,5 +1,5 @@
 export async function getContacts() {
-    const response = await fetch('http://localhost:3307/contacts');
+    const response = await fetch('http://backend:3307/contacts');
     if (!response.ok) throw new Error('Erreur API');
     return await response.json();
 }
